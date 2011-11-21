3a4d573536bf9a99af5b54efcb3010008178ec76
Sebastian Geiger <sebastian.geiger@gmail.com>
2011-11-21 09:39:07 +0100

    Added gitconfig file and symlinking it in setup.

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ git submodule init
 git submodule update
 ln -s profile ~/.profile
 ln -s modules/connection_strings/connect_ssh.sh ~/.connect_ssh
-
+ln -s gitconfig ~/.gitconfig
 ln -s vimrc ~/.vimrc
